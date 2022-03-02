001527cb9176874a8247174aa308a3e22567d9ca
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2022-03-02 03:01:30 +0000

    Fix/setup.py (#4)
    * feat/workflows
    
    * fix/setup.py
    
    Co-authored-by: jarbasai <jarbasai@mailfence.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,56 @@
 #!/usr/bin/env python3
 from setuptools import setup
+import os
+
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
 
 # NOTE: if you want compatibility with mycroft-core you CAN NOT move __init__.py
 # skills installed from source are git cloned and depend on a rigid structure
 
 # skill_id=package_name:SkillClass
-PLUGIN_ENTRY_POINT = 'mycroft-stop.mycroftai=ovos_plugin_skill_stop:StopSkill'
-# in this case the skill_id is defined to purposefully replace the mycroft version of the skill,
-# or rather to be replaced by it in case it is present. all skill directories take precedence over plugin skills
+PLUGIN_ENTRY_POINT = 'ovos-skill-stop.OpenVoiceOS=ovos_skill_stop:StopSkill'
 
 setup(
     # this is the package name that goes on pip
-    name='ovos-plugin-skill-stop',
-    version='0.0.1',  # don't forget to follow proper versioning and increment on each release!
+    name='ovos-skill-stop',
+    version=get_version(),
     description='OVOS stop skill plugin',
     url='https://github.com/OpenVoiceOS/skill-ovos-stop',
     author='JarbasAi',
@@ -21,9 +59,9 @@ setup(
 
     # empty str define the package as the root
     # note the usage of _ instead of - for the imports
-    package_dir={"ovos_plugin_skill_stop": ""},
-    packages=['ovos_plugin_skill_stop'],
-    package_data={'ovos_plugin_skill_stop': ['locale/*', 'vocab/*', "dialog/*"]},
+    package_dir={"ovos_skill_stop": ""},
+    packages=['ovos_skill_stop'],
+    package_data={'ovos_skill_stop': ['locale/*', 'vocab/*', "dialog/*"]},
     include_package_data=True,
     keywords='ovos mycroft skill plugin',
     entry_points={'ovos.plugin.skill': PLUGIN_ENTRY_POINT}
